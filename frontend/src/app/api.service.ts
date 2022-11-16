1669688d9ced4983cfbc045146774a2eb92bca1c
IyedBecheikh <iyed.becheikh@itbs.tn>
2022-11-16 18:51:16 +0100

    Redirect

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http:HttpClient) { }
+  constructor(
+    private http:HttpClient,
+    private router:Router
+    ) { }
   registerUser(user:any){
     this.http.post('/api/register',user).subscribe({
       next: data => {
+        this.router.navigate(['/login'])
+        console.log("Welcome to the club! You can now log in.");
         console.log(data);
       }
     })
