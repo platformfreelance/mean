4bec9705f3d052ccaa08b049d3fa609017c3c3ba
alaa TURKI <alaa.turki@itbs.tn>
2022-11-18 19:15:51 +0100

    THE HOME PAGE .......

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -16,7 +16,7 @@ export class ApiService {
       next: data => {
         this.router.navigate(['/login'])
         console.log("Welcome to the club! You can now log in.");
-        console.log(data);
+        console.log(data);  
       }
     })
   }
@@ -24,9 +24,8 @@ export class ApiService {
     this.http.post('/api/login',user).subscribe({
       next: data => {
         console.log("You are now logged in.");
-        console.log("u are logged in");
-        // this.router.navigate(['/home'])
-        
+        this.router.navigate(['/'])
+        console.log(data);
       }
     })
   }
